84d9c51f24a3679a2848e85183f381650a11d142
Augusto Yunes <augusto.yunes@accenture.com>
2025-03-08 12:33:47 -0300

    adding activity detection and other messages

--- server/index.js
+++ server/index.js
@@ -25,8 +25,25 @@ const io = new Server(expressServer, {
 io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
 
+  // Upon connection - only to user
+  socket.emit('message', 'Welcome to the chat');
+
+  // Upon connection - to all other users
+  socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} connected`);
+
+  // Listening for a message event
   socket.on("message", data => {
     console.log(data);
     io.emit('message', `${socket.id.substring(0,5)}: ${data}`);
   });
+
+  // When user disconnects - to all others
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} disconnected`);
+  });
+
+  // Listening for activity event
+  socket.on('activity', name => {
+    socket.broadcast.emit('activity', name);
+  })
 });
--- server/public/app.js
+++ server/public/app.js
@@ -18,7 +18,23 @@ document.querySelector('form').addEventListener('submit', sendMessage);
 // Listen for messages
 
 socket.on('message', ( data ) => {
+  activity.textContent = '';
   const li = document.createElement('li');
   li.textContent = data;
   document.querySelector('ul').appendChild(li);
+})
+
+// Listen for activity
+let activityTimer;
+socket.on('activity', (name) => {
+  activity.textContent = `${name} is typing...`;
+  // Clear the message after some time
+  clearTimeout(activityTimer);
+  activityTimer = setTimeout(() => {
+    activity.textContent = '';
+  }, 3000);
+})
+
+msgInput.addEventListener('keypress', () => {
+  socket.emit('activity', socket.id.substring(0, 5));
 })
